d53e5ac54cd7383cb9ae131cd02c340a7e152785
unknown <adam9312@comcast.net>
2018-05-01 20:28:04 -0700

    burger

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'pesto123',
-    database : 'burgers_db'
+    host     : 'ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'rf8bo1x6jhgcnmyu',
+    password : 'ti8qtnqkaygt7u02',
+    database : 'ypvkclmu0hzhofjg'
   });
 };
 
